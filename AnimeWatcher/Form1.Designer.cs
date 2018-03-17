b0565c208e69ce176506a982ec85bf8d30300b39
AniCSharp <37463284+AniCSharp@users.noreply.github.com>
2018-03-16 22:58:12 -0700

    Version 0.2

---
+++ AnimeWatcher/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace AnimeWatcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.listAnimes = new System.Windows.Forms.ListBox();
+            this.listEpisodes = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(12, 28);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(880, 703);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            // 
+            // listAnimes
+            // 
+            this.listAnimes.FormattingEnabled = true;
+            this.listAnimes.Items.AddRange(new object[] {
+            "Tokyo Ghoul"});
+            this.listAnimes.Location = new System.Drawing.Point(898, 28);
+            this.listAnimes.Name = "listAnimes";
+            this.listAnimes.Size = new System.Drawing.Size(315, 706);
+            this.listAnimes.TabIndex = 1;
+            this.listAnimes.SelectedIndexChanged += new System.EventHandler(this.listAnimes_SelectedIndexChanged);
+            // 
+            // listEpisodes
+            // 
+            this.listEpisodes.FormattingEnabled = true;
+            this.listEpisodes.Location = new System.Drawing.Point(1219, 28);
+            this.listEpisodes.Name = "listEpisodes";
+            this.listEpisodes.Size = new System.Drawing.Size(315, 355);
+            this.listEpisodes.TabIndex = 2;
+            this.listEpisodes.SelectedIndexChanged += new System.EventHandler(this.listEpisodes_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1033, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Anime List";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1344, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Episode List";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(3, 104);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(79, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Dark Mode";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Location = new System.Drawing.Point(12, 610);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(880, 124);
+            this.panel1.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(443, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(183, 33);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Play";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(254, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(183, 33);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Pause";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1547, 746);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listEpisodes);
+            this.Controls.Add(this.listAnimes);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Ani\'s Anime Watcher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.ListBox listAnimes;
+        private System.Windows.Forms.ListBox listEpisodes;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ AnimeWatcher/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.DirectX.AudioVideoPlayback;
+using System.IO;
+
+namespace AnimeWatcher
+{
+    public partial class Form1 : Form
+    {
+        private void bindingNavigator1_RefreshItems(object sender, EventArgs e)
+        {
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void PlayFile(String url)
+        {
+            axWindowsMediaPlayer1.URL = url;
+            axWindowsMediaPlayer1.Ctlcontrols.play();
+        }
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+            this.BackColor = Color.FromArgb(50, 50, 50);
+            listAnimes.BackColor = Color.FromArgb(60, 60, 60);
+            listAnimes.ForeColor = Color.White;
+            label1.ForeColor = Color.White;
+            label2.ForeColor = Color.White;
+            listEpisodes.BackColor = Color.FromArgb(60, 60, 60);
+            listEpisodes.ForeColor = Color.White;
+            panel1.BackColor = Color.FromArgb(60, 60, 60);
+            checkBox1.ForeColor = Color.White;
+            button1.BackColor = Color.FromArgb(60, 60, 60);
+            button1.ForeColor = Color.White;
+            button2.BackColor = Color.FromArgb(60, 60, 60);
+            button2.ForeColor = Color.White;
+            checkBox1.Checked = true;
+            CheckForIllegalCrossThreadCalls = false;
+            PlayFile(@"Videos\Loading.mp4");
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listEpisodes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listEpisodes.SelectedIndex == 0)
+            {
+                PlayFile("http://cache1.animepremium.tv:558/cache/537Qlxmdj28Vsuevqy_Xrw/1521385345/uoqpdmer0fbv.html.mp4");
+            }
+            else if (listEpisodes.SelectedIndex == 1)
+            {
+                PlayFile("http://cache1.animepremium.tv:558/cache/GKlYZV9YLARI8qJcpGqVAw/1521385416/jinxlirt5dew.html.mp4");
+            }
+            else if (listEpisodes.SelectedIndex == 2)
+            {
+                PlayFile("http://cache1.animepremium.tv:558/cache/jMTM1ALWazP5AhgVoZRSmw/1521386168/o95sy52bd2kr.html.mp4");
+            }
+            else if (listEpisodes.SelectedIndex == 3)
+            {
+                PlayFile("http://cache1.animepremium.tv:558/cache/c92sn_Mn2CedAmrlMVlT2Q/1521386220/gitsdkokmdfb.html.mp4");
+            }
+            else if (listEpisodes.SelectedIndex == 4)
+            {
+                PlayFile("http://cache1.animepremium.tv:558/cache/wjSsh1wXpLJ8MLjKnRYJwg/1521386270/xrtousjrtx6e.html.mp4");
+            }
+            else if (listEpisodes.SelectedIndex == 5)
+            {
+                PlayFile("http://cache1.animepremium.tv:558/cache/cW-3Anpw4Qupg1MijxZzxw/1521386323/8gofcswxk89b.html.mp4");
+            }
+            else if (listEpisodes.SelectedIndex == 6)
+            {
+                PlayFile("http://cache1.animepremium.tv:558/cache/phdMEYXcNsJBYGsJjRLQNw/1521402147/ojy89rsoxj28.html.mp4");
+            }
+            else if (listEpisodes.SelectedIndex == 7)
+            {
+                PlayFile("http://cache1.animepremium.tv:558/cache/flBlM7Z4l6oSWZWZY99xrQ/1521402193/mpnif8mxp1xa-650x370.html.mp4");
+            }
+            else if (listEpisodes.SelectedIndex == 8)
+            {
+                PlayFile("http://cache1.animepremium.tv:558/cache/YY-vc25zeDID3us2EBAzgQ/1521402228/udr6grb5lrc6.html.mp4");
+            }
+            else if (listEpisodes.SelectedIndex == 9)
+            {
+                PlayFile("http://cache1.animepremium.tv:558/cache/rzcnDV_TN_Y_bEviZBlJVw/1521402251/afkefsae1rc8.html.mp4");
+            }
+            else if (listEpisodes.SelectedIndex == 10)
+            {
+                PlayFile("http://cache1.animepremium.tv:558/cache/JKI4EFsykN_Z66Lv68h0iw/1521402277/krlky955395x-650x370.html.mp4");
+            }
+            else if (listEpisodes.SelectedIndex == 11)
+            {
+                PlayFile("http://cache1.animepremium.tv:558/cache/4EpUNMWhXEZUSxR4e1eAXQ/1521402296/on20i27dgmud.html.mp4");
+            }
+            else
+            {
+                axWindowsMediaPlayer1.close();
+                PlayFile(@"Videos\Loading.mp4");
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == false)
+            {
+                this.BackColor = Color.White;
+                listAnimes.BackColor = Color.White;
+                listAnimes.ForeColor = Color.Black;
+                label1.ForeColor = Color.Black;
+                label2.ForeColor = Color.Black;
+                listEpisodes.BackColor = Color.White;
+                listEpisodes.ForeColor = Color.Black;
+                panel1.BackColor = Color.White;
+                checkBox1.ForeColor = Color.Black;
+                button1.BackColor = Color.White;
+                button1.ForeColor = Color.Black;
+                button2.BackColor = Color.White;
+                button2.ForeColor = Color.Black;
+            }
+            else
+            {
+                this.BackColor = Color.FromArgb(50, 50, 50);
+                listAnimes.BackColor = Color.FromArgb(60, 60, 60);
+                listAnimes.ForeColor = Color.White;
+                label1.ForeColor = Color.White;
+                label2.ForeColor = Color.White;
+                listEpisodes.BackColor = Color.FromArgb(60, 60, 60);
+                listEpisodes.ForeColor = Color.White;
+                panel1.BackColor = Color.FromArgb(60, 60, 60);
+                checkBox1.ForeColor = Color.White;
+                button1.BackColor = Color.FromArgb(60, 60, 60);
+                button1.ForeColor = Color.White;
+                button2.BackColor = Color.FromArgb(60, 60, 60);
+                button2.ForeColor = Color.White;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.pause();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.play();
+        }
+
+        private void listAnimes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listAnimes.SelectedIndex == 0)
+            {
+                listEpisodes.Items.Clear();
+                listEpisodes.Items.Add("Episode 1");
+                listEpisodes.Items.Add("Episode 2");
+                listEpisodes.Items.Add("Episode 3");
+                listEpisodes.Items.Add("Episode 4");
+                listEpisodes.Items.Add("Episode 5");
+                listEpisodes.Items.Add("Episode 6");
+                listEpisodes.Items.Add("Episode 7");
+                listEpisodes.Items.Add("Episode 8");
+                listEpisodes.Items.Add("Episode 9");
+                listEpisodes.Items.Add("Episode 10");
+                listEpisodes.Items.Add("Episode 11");
+                listEpisodes.Items.Add("Episode 12");
+            }
+        }
+    }
+}
